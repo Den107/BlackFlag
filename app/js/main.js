22187255e7ee2dc134fbd5c83670e65dc632846a
Den107 <deniskrv@gmail.com>
2020-12-22 00:54:41 +0300

    Сделал половину шапки

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
-
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="arrl"></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="arrr"></img>',
+        asNavFor: '.slider-dots'
+    });
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider'
+    });
 });
